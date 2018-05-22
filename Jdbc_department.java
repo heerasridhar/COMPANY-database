22251c0c505f84c4edad5be4bf6da2eff99e452a
HEERA SRIDHAR <38568704+heerasridhar@users.noreply.github.com>
2018-05-21 22:08:37 -0500

    Add files via upload

---
+++ Jdbc_department.java
@@ -0,0 +1,86 @@
+package klab1;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dell
+ */
+public class Jdbc_department {
+    public static void main(String[] args) throws IOException, SQLException{
+		 Connection conn;
+		
+		 try {
+	         Class.forName("oracle.jdbc.OracleDriver");
+	      }
+	      catch (ClassNotFoundException e) {
+	         System.out.println("Could not load the driver.");
+	      }
+		 
+		 conn = DriverManager.getConnection("jdbc:oracle:oci:@localhost:1521:xe", "system", "system");
+                 //conn = DriverManager.getConnection("jdbc:oracle:thin:vro8522/Apple123@omega:1521:cse1");
+                 
+                 BufferedReader br2 = new BufferedReader(new FileReader("C:\\Users\\dell\\Downloads\\datafiles\\DEPARTMENT.txt"));
+    
+     /* code for loading data to the department table*/
+                   String text2,Dname,Mgr_strt_date,Mgr_ssn_temp;
+                   int Dnumber,Mgr_ssn,Mgr_ssn_temp2;
+                
+                    while ((text2 = br2.readLine()) != null) {
+			String spltline2[] = text2.split(",");
+                            	Dname = spltline2[0].replaceAll("'","");
+                                    Dname = Dname.replaceAll(" ","");
+                                Dnumber = Integer.parseInt(spltline2[1].replaceAll(" ",""));
+                                
+                                Mgr_ssn_temp = spltline2[2].replaceAll("'","");
+                                    Mgr_ssn_temp = Mgr_ssn_temp.replaceAll(" ","");
+                                    if(!("null".equals(Mgr_ssn_temp)) )
+                                    {
+                                    Mgr_ssn_temp2 = Integer.parseInt(Mgr_ssn_temp);
+                                    Mgr_ssn = Mgr_ssn_temp2;
+                                    }
+                                    else
+                                        {   Mgr_ssn = 0; 
+                                        }
+                                    
+                                Mgr_strt_date = spltline2[3].replaceAll("'","");
+                                    Mgr_strt_date = Mgr_strt_date.replaceAll(" ","");
+                                
+                                
+                            System.out.println(Dname+Dnumber+Mgr_ssn+Mgr_strt_date);
+                                 String query2 = "INSERT INTO DEPARTMENT values(?,?,?,?)";
+                                PreparedStatement ps2 = conn.prepareStatement(query2);
+                                ps2.setString(1,Dname);
+                                ps2.setInt(2,Dnumber);
+                                if (Mgr_ssn == 0) {
+                                    ps2.setNull(3, Types.INTEGER);                           
+                                     } 
+                                else {
+                                    ps2.setInt(3,Mgr_ssn);
+                                     }
+                                ps2.setString(4,Mgr_strt_date);
+                        try {
+                        ps2.execute();
+                        }catch (SQLException e ) {
+		            System.out.println("catch block ps2");
+		            e.printStackTrace();
+		        } 
+                                
+                    }
+    
+}
+}
---
+++ project.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klab1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author dell
+ */
+public class project {
+    public static void main(String[] args) throws IOException, SQLException{
+		 Connection conn;
+		
+		 try {
+	         Class.forName("oracle.jdbc.OracleDriver");
+	      }
+	      catch (ClassNotFoundException e) {
+	         System.out.println("Could not load the driver.");
+	      }
+		 
+		 conn = DriverManager.getConnection("jdbc:oracle:oci:@localhost:1521:xe", "system", "system");
+                 BufferedReader br4 = new BufferedReader(new FileReader("C:\\Users\\dell\\Downloads\\datafiles\\PROJECT.txt"));
+
+                  String text4,Pname,Plocation,Pnum_temp;
+                    int Pnumber,Dnumb;
+                        while ((text4 = br4.readLine()) != null) {
+                            String spltline4[] = text4.split(",");
+                                Pname = spltline4[0].replaceAll("'","");
+                                Pname = Pname.replaceAll(" ","");
+                                Pnum_temp = spltline4[1].replaceAll(" ","");
+                                Pnumber = Integer.parseInt(Pnum_temp);
+                                Plocation = spltline4[2].replaceAll("'","");
+                                Plocation = Plocation.replaceAll(" ","");
+                                Dnumb = Integer.parseInt(spltline4[3].replaceAll(" ",""));
+                               System.out.println(Pname+Pnumber+Plocation+Dnumb);
+                               String query4 = "INSERT INTO PROJECT values(?,?,?,?)";
+                               PreparedStatement ps4 = conn.prepareStatement(query4);
+                                ps4.setString(1,Pname);
+                                ps4.setInt(2,Pnumber);
+                                ps4.setString(3,Plocation);
+                                ps4.setInt(4,Dnumb);
+                       try {
+                        ps4.execute();
+                        //System.out.println("record inserted");
+                        }catch (SQLException e ) {
+		            System.out.println("catch block ps4");
+		            e.printStackTrace();
+                            }
+                        }
+    
+    }
+}
